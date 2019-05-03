0c21a15cace0199681544a443eb81112165b11ed
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-05-03 15:15:13 -0500

    Updated readme

--- hash-tables/ex2/hashtable.c
+++ hash-tables/ex2/hashtable.c
@@ -18,9 +18,10 @@ unsigned int hash(char *str, int max)
 {
   unsigned long hash = 5381;
   int c;
-  unsigned char * u_str = (unsigned char *)str;
+  unsigned char *u_str = (unsigned char *)str;
 
-  while ((c = *u_str++)) {
+  while ((c = *u_str++))
+  {
     hash = ((hash << 5) + hash) + c;
   }
 
@@ -29,7 +30,8 @@ unsigned int hash(char *str, int max)
 
 void destroy_pair(LinkedPair *pair)
 {
-  if (pair != NULL) free(pair);
+  if (pair != NULL)
+    free(pair);
 }
 
 HashTable *create_hash_table(int capacity)
@@ -48,14 +50,18 @@ void hash_table_insert(HashTable *ht, char *key, char *value)
   LinkedPair *current_pair = ht->storage[index];
   LinkedPair *last_pair;
 
-  while (current_pair != NULL && strcmp(current_pair->key, key) != 0) {
+  while (current_pair != NULL && strcmp(current_pair->key, key) != 0)
+  {
     last_pair = current_pair;
     current_pair = last_pair->next;
   }
 
-  if (current_pair != NULL) {
+  if (current_pair != NULL)
+  {
     current_pair->value = value;
-  } else {
+  }
+  else
+  {
     LinkedPair *new_pair = create_pair(key, value);
     new_pair->next = ht->storage[index];
     ht->storage[index] = new_pair;
@@ -69,20 +75,26 @@ void hash_table_remove(HashTable *ht, char *key)
   LinkedPair *current_pair = ht->storage[index];
   LinkedPair *previous_pair = NULL;
 
-  while (current_pair != NULL && strcmp(current_pair->key, key) != 0) {
+  while (current_pair != NULL && strcmp(current_pair->key, key) != 0)
+  {
     previous_pair = current_pair;
     current_pair = current_pair->next;
   }
 
-  if (current_pair == NULL) {
+  if (current_pair == NULL)
+  {
 
     fprintf(stderr, "Unable to remove entry with key: %s\n", key);
+  }
+  else
+  {
 
-  } else {
-
-    if (previous_pair == NULL) {  // Removing the first element in the Linked List
+    if (previous_pair == NULL)
+    { // Removing the first element in the Linked List
       ht->storage[index] = current_pair->next;
-    } else {
+    }
+    else
+    {
       previous_pair->next = current_pair->next;
     }
 
@@ -96,8 +108,10 @@ char *hash_table_retrieve(HashTable *ht, char *key)
 
   LinkedPair *current_pair = ht->storage[index];
 
-  while (current_pair != NULL) {
-    if (strcmp(current_pair->key, key) == 0) {
+  while (current_pair != NULL)
+  {
+    if (strcmp(current_pair->key, key) == 0)
+    {
       return current_pair->value;
     }
     current_pair = current_pair->next;
@@ -111,9 +125,11 @@ void destroy_hash_table(HashTable *ht)
   LinkedPair *current_pair;
   LinkedPair *pair_to_destroy;
 
-  for (int i = 0 ; i < ht->capacity ; i++) {
+  for (int i = 0; i < ht->capacity; i++)
+  {
     current_pair = ht->storage[i];
-    while(current_pair != NULL) {
+    while (current_pair != NULL)
+    {
       pair_to_destroy = current_pair;
       current_pair = current_pair->next;
       destroy_pair(pair_to_destroy);
@@ -129,9 +145,11 @@ HashTable *hash_table_resize(HashTable *ht)
   HashTable *new_ht = create_hash_table(2 * ht->capacity);
 
   LinkedPair *current_pair;
-  for (int i = 0 ; i < ht->capacity ; i++) {
+  for (int i = 0; i < ht->capacity; i++)
+  {
     current_pair = ht->storage[i];
-    while (current_pair != NULL) {
+    while (current_pair != NULL)
+    {
       hash_table_insert(new_ht, current_pair->key, current_pair->value);
       current_pair = current_pair->next;
     }
